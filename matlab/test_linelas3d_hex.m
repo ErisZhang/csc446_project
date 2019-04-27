f059f133165df67e4df32c8c77e462865ab9b2b3
eriszhang <jiayieris.zhang@mail.utoronto.ca>
2019-04-27 12:05:50 -0400

    hex stress seems to work; vis problematic

--- matlab/test_linelas3d_hex.m
+++ matlab/test_linelas3d_hex.m
@@ -3,10 +3,10 @@ format shortG;
 warning('off','all');
 set_project_paths();
 
-[V,T,F] = readMESH('../data/archbridge_tiny.mesh');
+[V,T,F] = readMESH('../data/bunny_tiny.mesh');
 
 
-[W,BC,DV,Q,r] = voxelize(V,F,40);
+[W,BC,DV,Q,r] = voxelize(V,F,30);
 
 
 load = [0; -9.8; 0];
@@ -14,18 +14,44 @@ load = [0; -9.8; 0];
 [VM,P,C,data] = linelas3d_hexahedron(W,load,r,DV,V,T);
 
 ori = DV(1,:);
+all_V = zeros(size(VM,1),3);
+all_F = [];
 
-% for i = 1:size(Q,1)
-%     for j = 1:4
-%         idx = floor((DV(Q(i,j),:)-ori)./r)+1;
-%         idx = index_ijk_to_p(idx);
-%         if idx > size(VM)
-%             disp("problem here");
-%         end
-%     end
-% end
+for i = 1:size(P,1)
+    for j = 1:size(P,2)
+        for k = 1:size(P,3)
+            idx = P(i,j,k);
+            if idx ~= 0
+                v_pos = [j-1,i-1,k-1].*r+ori;
+                all_V(idx,:) = v_pos;
+            end
+        end
+    end
+end
 
-% 
-% options.face_vertex_color = VM;
-% plot_mesh(V,F,options);
-% colormap('jet');
+for i = 1:size(Q,1)
+    p_idx = [];
+    for j = 1:4
+        idx = floor((DV(Q(i,j),:)-ori)./r)+1;
+        p =  P(idx(2),idx(1),idx(3));
+        p_idx = [p_idx p];       
+    end
+    if any(p_idx==0)
+        continue;
+    end
+    all_F = [all_F
+             p_idx(1:3)
+             [p_idx(3) p_idx(4) p_idx(1)]];
+end
+
+
+for i = 1:size(VM,1)
+    if VM(i) > 0.75*max(VM)
+        VM(i) = 0.75*max(VM);
+    end
+end
+
+
+options.face_vertex_color = VM;
+plot_mesh(all_V,all_F,options);
+colormap('jet');
